fce6278ef4d86360c1aef464d05ba5e4fadbae65
Arc Aaron <cranoraa@gmail.com>
2026-05-18 05:53:33 +0800

    Update settings.py

--- backend/school_portal/settings.py
+++ backend/school_portal/settings.py
@@ -103,11 +103,12 @@ CHANNEL_LAYERS = {
 
 
 # Database
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
